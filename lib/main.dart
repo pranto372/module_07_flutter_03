435cd5c68c0f457885a82e787790889688d34dc1
pranto372 <prantodeb62@gmail.com>
2023-09-20 10:42:22 +0600

    Aspect Ratio

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,22 @@ class MyHomePage extends StatelessWidget{
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+      ),
+      body: Container(
+        height: 200,
+        width: double.infinity,
+        color: Colors.red,
+          alignment: Alignment.center,
+          child: AspectRatio(
+            aspectRatio: 16/9,
+            child: Container(
+              color: Colors.black,
+            ),
+        ),
       ),
     );
   }
