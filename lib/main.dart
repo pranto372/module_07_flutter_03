3c6a0fcc3665d85adc208c0dc6f76671bbab5682
pranto372 <prantodeb62@gmail.com>
2023-09-24 23:38:34 +0600

    Module 07 Live Test

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,92 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-   return const MaterialApp(
-     title: "Counter App",
-     home: MyHomePage(),
-   );
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
   }
 }
 
-class MyHomePage extends StatefulWidget{
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return MyHomePageUI();
-  }
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class MyHomePageUI extends State<MyHomePage>{
-
-  int CountNumber = 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int count = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Counter App"),
-      ),
-      body: Center(
-        child: Text(CountNumber.toString(), style: TextStyle(fontSize: 30),),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){
-          setState(() {
-            CountNumber = CountNumber + 1;
-          });
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text("Counter App"),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Count:", style: TextStyle(fontSize: 24)),
+            Text(count.toString(),
+                style:
+                    const TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        setState(() {
+                          count++;
+                        });
+                        if (count >= 5) {
+                          showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                    title: Text("Button Pressed $count time"),
+                                    actions: [
+                                      TextButton(
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          },
+                                          child: const Text("Close")),
+                                    ],
+                                  ));
+                        }
+                      });
+                    },
+                    child: const Icon(Icons.add)),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (count >= 1) {
+                          count = count - 1;
+                        }
+                      });
+                    },
+                    child: const Icon(Icons.remove)),
+              ],
+            )
+          ],
+        ));
   }
 }
